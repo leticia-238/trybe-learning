037b7f6836b501c89eb46cb97b08b77b93cee628
Leticia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2021-11-28 20:33:54 -0300

    Primeiro exercicio completo

---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-04_Introdução-à-JavaScript-e-Lógica-de-Programação/Primeiros-Passos/ex-0.1/scripts.js
@@ -0,0 +1,12 @@
+const a = 864;
+const b = 54;
+
+let soma = a + b;
+let sub = a - b;
+let multi = a * b;
+let div = a / b;
+let mod = a % b;
+
+console.log(
+  `soma: ${soma} subtração: ${sub} multiplicação: ${multi} divisão: ${div} módulo: ${mod}`
+);
