50d2e2a28819a476055eb57c968ec5a3a585538b
Leticia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2021-11-28 20:47:56 -0300

    Segundo exercicio completo

---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-04_Introdução-à-JavaScript-e-Lógica-de-Programação/Primeiros-Passos/ex-0.2/scripts.js
@@ -0,0 +1,13 @@
+const a = 65;
+const b = 98;
+let result = null;
+
+if (a > b) {
+  result = a;
+} else if (a === b) {
+  result = "os dois valores são iguais";
+} else {
+  result = b;
+}
+
+console.log(result);
