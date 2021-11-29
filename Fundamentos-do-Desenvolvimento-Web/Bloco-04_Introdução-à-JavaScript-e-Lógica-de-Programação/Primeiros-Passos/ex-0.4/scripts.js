c8c3b6076ec3b510137858f64db9503261c9107f
Leticia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2021-11-28 21:15:28 -0300

    Quarto exercicio completo

---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-04_Introdução-à-JavaScript-e-Lógica-de-Programação/Primeiros-Passos/ex-0.4/scripts.js
@@ -0,0 +1,12 @@
+const a = -223;
+let result = null;
+
+if (a > 0) {
+  result = "positive";
+} else if (a < 0) {
+  result = "nagative";
+} else if (a === 0) {
+  result = "zero";
+}
+
+console.log(result);
