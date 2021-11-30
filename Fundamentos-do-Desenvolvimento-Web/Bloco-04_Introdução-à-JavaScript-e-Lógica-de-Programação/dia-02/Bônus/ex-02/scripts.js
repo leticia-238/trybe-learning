8fce027eb2de83522c5503d948529cce74096f96
Leticia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2021-11-30 12:40:38 -0300

    Segundo exercicio bonus realizado

---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-04_Introdução-à-JavaScript-e-Lógica-de-Programação/dia-02/Bônus/ex-02/scripts.js
@@ -0,0 +1,17 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (
+  let numeroDeIteracoes = 1;
+  numeroDeIteracoes < numbers.length;
+  numeroDeIteracoes += 1
+) {
+  for (let i = 0; i < numbers.length - numeroDeIteracoes; i += 1) {
+    if (numbers[i] < numbers[i + 1]) {
+      let aux = numbers[i];
+      numbers[i] = numbers[i + 1];
+      numbers[i + 1] = aux;
+    }
+  }
+}
+
+console.log(numbers);
