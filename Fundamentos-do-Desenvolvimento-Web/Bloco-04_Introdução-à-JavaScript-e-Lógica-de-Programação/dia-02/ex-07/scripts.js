2591ac325fff472403ecbc2e0ae6e896bb82dd59
Leticia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2021-11-30 11:32:00 -0300

    Setimo exercicio realizado

---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-04_Introdução-à-JavaScript-e-Lógica-de-Programação/dia-02/ex-07/scripts.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorNumero = numbers[1];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  let num = numbers[index];
+
+  if (num < menorNumero) {
+    menorNumero = num;
+  }
+}
+
+console.log(menorNumero);
