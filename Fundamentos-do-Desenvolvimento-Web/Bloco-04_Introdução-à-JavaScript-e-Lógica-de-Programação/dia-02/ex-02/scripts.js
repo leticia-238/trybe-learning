04f28e1e4ebd9ce295981ad801f0e4c145b223dc
Leticia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2021-11-30 10:15:52 -0300

    Segundo exercicio realizado

---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-04_Introdução-à-JavaScript-e-Lógica-de-Programação/dia-02/ex-02/scripts.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = null;
+
+for (let index = 0; index < numbers.length; index++) {
+  soma += numbers[index];
+}
+
+console.log("Soma:", soma);
