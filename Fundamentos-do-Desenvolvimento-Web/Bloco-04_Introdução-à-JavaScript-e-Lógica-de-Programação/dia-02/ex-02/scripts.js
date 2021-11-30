5a21d96b0ab4224ff6617bb39a83e1620170be97
Leticia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2021-11-30 11:28:14 -0300

    Corrige os acrecimos no for de index++ para index += 1

--- Fundamentos-do-Desenvolvimento-Web/Bloco-04_Introdução-à-JavaScript-e-Lógica-de-Programação/dia-02/ex-02/scripts.js
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-04_Introdução-à-JavaScript-e-Lógica-de-Programação/dia-02/ex-02/scripts.js
@@ -1,7 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = null;
 
-for (let index = 0; index < numbers.length; index++) {
+for (let index = 0; index < numbers.length; index += 1) {
   soma += numbers[index];
 }
 
--- Fundamentos-do-Desenvolvimento-Web/Bloco-04_Introdução-à-JavaScript-e-Lógica-de-Programação/dia-02/ex-04/scripts.js
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-04_Introdução-à-JavaScript-e-Lógica-de-Programação/dia-02/ex-04/scripts.js
@@ -2,7 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = null;
 let media = null;
 
-for (let index = 0; index < numbers.length; index++) {
+for (let index = 0; index < numbers.length; index += 1) {
   soma += numbers[index];
 }
 
