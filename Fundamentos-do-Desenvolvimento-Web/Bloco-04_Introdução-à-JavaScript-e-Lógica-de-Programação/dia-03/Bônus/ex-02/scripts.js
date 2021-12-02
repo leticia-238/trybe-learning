c93dfc593aa6d6c036b2be90147e55a423521fbe
Leticia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2021-12-01 23:24:08 -0300

    Terceiro exercicio bonus completo

--- Fundamentos-do-Desenvolvimento-Web/Bloco-04_Introdução-à-JavaScript-e-Lógica-de-Programação/dia-03/Bônus/ex-02/scripts.js
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-04_Introdução-à-JavaScript-e-Lógica-de-Programação/dia-03/Bônus/ex-02/scripts.js
@@ -1,8 +1,12 @@
-let starNumber = 8;
+let number = 8;
 let line = "";
 
-for (let num = 1; num <= starNumber; num += 1) {
-  for (let row = 0; row < num; row += 1) {
+for (let n = 1; n <= number; n += 1) {
+  for (let countSpace = n; countSpace < number; countSpace += 1) {
+    line += " ";
+  }
+  let starNumber = number - line.length;
+  for (let countStar = 0; countStar < starNumber; countStar += 1) {
     line += "*";
   }
   console.log(line);
