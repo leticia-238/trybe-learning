106d9f561f031f002f58c995f9326fa509448dfd
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-01-16 18:49:45 -0300

    primeiro exercicio

---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-07_Introdução_à_JavaScript_ES6_e_Testes_Unitários/let_const_arrow-functions_e_template-literals/ex-01.js
@@ -0,0 +1,20 @@
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    return `${ifScope} ótimo, fui utilizada no escopo !`;
+
+  }
+  return 'Não devo ser utilizada fora meu escopo (else)';
+}
+
+console.log(testingScope(true));
+console.log(testingScope(false))
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortNumbers = (array) => {
+  let numbers = array.sort((a, b) => a - b).join(', ')
+  return `Os números ${numbers} se encontram ordenados de forma crescente!`
+}
+
+console.log(sortNumbers(oddsAndEvens)); 
